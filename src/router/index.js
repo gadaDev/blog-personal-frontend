fe48b618a7431ec5e1bac0e4e92d08d215b36425
Vĩnh Hải <gada121982@gmail.com>
2020-04-27 12:42:18 +0700

    setup templet

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -8,23 +7,29 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: () => import("../views/home.vue"),
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    path: "/book",
+    name: "book",
+    component: () => import("../views/book.vue"),
+  },
+  {
+    path: "/chat",
+    name: "chat",
+    component: () => import("../views/chat.vue"),
+  },
+  {
+    path: "auth",
+    name: "auth",
+    component: () => import("../views/auth.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
---
+++ src/views/book.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>book</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
